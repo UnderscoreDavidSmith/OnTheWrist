dbb631a8efc8184c2c6bef3a217f4725e07f1dd7
David Smith <git@david-smith.org>
2020-04-24 20:12:43 -0400

    Initial Commit

---
+++ OnTheWrist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnTheWrist
+//
+//  Created by David Smith on 4/24/20.
+//  Copyright © 2020 Cross Forward Consulting, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
